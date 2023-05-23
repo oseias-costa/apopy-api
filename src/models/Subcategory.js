4a555f9d77765d348dd2aecf4a38da937ab46ee5
Oséias Costa <oseiasc2@gmail.com>
2023-05-23 20:12:03 +0000

    remove mongoose

--- src/models/Subcategory.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema, defaut: mongoose } = require("mongoose");
-
-const SubcategorySchema = new Schema({
-  name: String,
-  category: Schema.Types.ObjectId,
-});
-
-module.exports = model("Subcategory", SubcategorySchema);
