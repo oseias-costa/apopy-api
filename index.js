200c8fd4be42b0c618fad0e6b4a56ea782c02036
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-05-24 23:36:50 -0300

    express apollo v4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-//const express = require("express");
+const express = require("express");
 const { ApolloServer } = require("@apollo/server");
-const { startStandaloneServer } = require("@apollo/server/standalone");
-//const { expressMiddleware } = require("@apollo/server/express4");
+//const { startStandaloneServer } = require("@apollo/server/standalone");
+const { expressMiddleware } = require("@apollo/server/express4");
 const {
   ApolloServerPluginDrainHttpServer,
 } = require("@apollo/server/plugin/drainHttpServer");
@@ -9,10 +9,10 @@ const { ConnectedMongoDB } = require("./src/services/mongodb");
 
 //const dotenv = require('dotenv')
 //dotenv.config()
-//const http = require("http");
+const http = require("http");
 const path = require("path");
-//const cors = require("cors");
-//const { json } = require("body-parser");
+const cors = require("cors");
+const { json } = require("body-parser");
 
 const { mergeResolvers, mergeTypeDefs } = require("@graphql-tools/merge");
 const { loadFilesSync } = require("@graphql-tools/load-files");
@@ -23,26 +23,26 @@ const typesArray = loadFilesSync(path.join(__dirname, "src/types"));
 const typeDefs = mergeTypeDefs(typesArray);
 const resolvers = mergeResolvers(resolverFiles);
 
-//const app = express();
-//const httpServer = http.createServer(app);
+const app = express();
+const httpServer = http.createServer(app);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  //plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
 async function StartApolloServer() {
   await ConnectedMongoDB();
-  const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-  });
+  //const { url } = await startStandaloneServer(server, {
+  //listen: { port: 4000 },
+  //});
 
-  console.log(`🚀  Server ready at: ${url}`);
-  //await server.start();
-  //app.use("/graphql", cors(), json(), expressMiddleware(server));
+  //console.log(`🚀  Server ready at: ${url}`);
+  await server.start();
+  app.use("/graphql", cors(), json(), expressMiddleware(server));
 
-  //await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
-  //console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`);
 }
 
 StartApolloServer();
