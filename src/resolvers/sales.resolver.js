1d5da0adeaabcb283affa9d6214fbc226c44cc0f
Oséias Costa <oseiasc2@gmail.com>
2023-08-08 20:43:10 +0000

    fix(sales.resolver): fixed BSON import

--- src/resolvers/sales.resolver.js
+++ src/resolvers/sales.resolver.js
@@ -1,22 +1,23 @@
 const { db } = require("../services/mongodb");
-const movimentStock  = require('../utils/stock/movimentStock')
-const reverseStockMoviment  = require('../utils/stock/reverseStockMoviment')
+const movimentStock = require("../utils/stock/movimentStock");
+const reverseStockMoviment = require("../utils/stock/reverseStockMoviment");
+const { BSON } = require("mongodb");
 
 module.exports = {
   Mutation: {
     async reverseSale(_, { saleInput }) {
-      return await reverseStockMoviment(saleInput, 'sale')
+      return await reverseStockMoviment(saleInput, "sale");
     },
 
-    async transferSale(_, { saleInput }){
-      return await movimentStock(saleInput, 'sale')
-    }
+    async transferSale(_, { saleInput }) {
+      return await movimentStock(saleInput, "sale");
+    },
   },
 
   Query: {
     async sales(parent, args, { user_id }) {
       const id = new BSON.ObjectId(user_id);
-      return await db.collection("sale").find({userId: id}).toArray();
+      return await db.collection("sale").find({ userId: id }).toArray();
     },
   },
 };
